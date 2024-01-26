e65c042b4229695f166065a6401afbead7ce16f8
demansh <demansh17@gmail.com>
2024-01-26 10:47:33 +0400

    init commit

---
+++ src/main/java/com/github/demansh/jaegerelkotel/JaegerElkOtelApplication.java
@@ -0,0 +1,13 @@
+package com.github.demansh.jaegerelkotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaegerElkOtelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaegerElkOtelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/demansh/jaegerelkotel/JaegerElkOtelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.demansh.jaegerelkotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaegerElkOtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
