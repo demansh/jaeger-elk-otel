5cec11cf0b86b50a6974f177eac2a15f98160308
demansh <demansh17@gmail.com>
2024-01-26 12:21:56 +0400

    set up maven structure

---
+++ api-service/src/main/java/com/github/demansh/ApiServiceApp.java
@@ -0,0 +1,11 @@
+package com.github.demansh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApp.class, args);
+    }
+}
---
+++ customers-service/src/main/java/com/github/demansh/CustomersServiceApp.java
@@ -0,0 +1,11 @@
+package com.github.demansh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersServiceApp.class, args);
+    }
+}
